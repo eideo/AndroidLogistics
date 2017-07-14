f2a1461be66efa9e96271bf6e0cf2c47c329e86e
Kenny <vip@avatarcn.com>
2017-07-14 20:00:55 +0800

    create_order

---
+++ app/src/main/java/com/kenny/logistics/ui/fragment/FragmentCrateOrder.java
@@ -0,0 +1,12 @@
+package com.kenny.logistics.ui.fragment;
+
+import com.kenny.logistics.R;
+import com.kenny.logistics.ui.base.BaseFragment;
+
+public class FragmentCrateOrder extends BaseFragment {
+    @Override
+    public int getContentViewId() {
+        return R.layout.fragment_mine;
+    }
+}
+	
